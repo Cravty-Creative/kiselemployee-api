8e1a5f6e79360499880d07411434a4974eecaa7a
Rizky A <44380444+Tsucie@users.noreply.github.com>
2022-12-18 19:05:04 +0700

    Add JWT Auth

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -1,8 +1,78 @@
 <?php
+
 namespace App\Http\Controllers;
+
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Models\Karyawan;
+use App\Models\User;
+use Illuminate\Support\Facades\Crypt;
+use Illuminate\Support\Facades\Validator;
+use Exception;
+use Illuminate\Support\Facades\Auth;
+
 class UserController extends Controller
 {
-    //
+  public function login(Request $request)
+  {
+    try {
+      // Validasi username dan password
+      $validator = Validator::make($request->all(), [
+        'username' => 'required|min:4',
+        'password' => 'required|min:6',
+      ], [
+        'required'  => ':attribute harus diisi',
+        'min'       => ':attribute minimal :min karakter',
+      ]);
+      // Kondisi validator gagal
+      if ($validator->fails()) {
+        throw new Exception($validator->errors()->first(), 400);
+      }
+      // Mengambil data user
+      $user = User::query()->where('username', '=', $request->username)->first();
+      // Kondisi Username benar
+      if ($user) {
+        // Kondisi Password benar
+        if (Crypt::decrypt($user->password) == $request->password) {
+          $token = Auth::attempt([
+            'username' => $request->username,
+            'password' => $request->password
+          ]);
+          if (!$token) {
+            throw new Exception("Login gagal", 500);
+          }
+          $response = array();
+          $karyawan = Karyawan::query()->where('user_id', '=', $user->id)->first();
+          if ($karyawan) {
+            $response = [
+              "token" => $token,
+              "username" => $user->username,
+              "name" => $karyawan->name,
+              "section" => $karyawan->section,
+              "job_title" => $karyawan->job_title
+            ];
+          }
+          else {
+            $response = [
+              "token" => $token,
+              "username" => $user->username
+            ];
+          }
+          return response()->json($response, 200);
+        }
+        // Kondisi Password salah
+        else {
+          throw new Exception("Password anda salah", 400);
+        }
+      }
+      // Kondisi Username salah
+      else {
+        throw new Exception("Username anda salah", 400);
+      }
+    } catch (Exception $ex) {
+      return response()->json([
+        'message' => $ex->getMessage()
+      ], $ex->getCode() ?? 500);
+    }
+  }
 }
---
+++ database/migrations/2022_12_18_094609_create_presensi_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePresensiTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        if (!Schema::hasTable('presensi')) {
+            Schema::create('presensi', function (Blueprint $table) {
+            $table->integer('id', true, true);
+            $table->integer('user_id', false, true);
+            $table->string('hari', 5);
+            $table->time('jam_masuk');
+            $table->time('jam_pulang')->nullable();
+            $table->string('status', 45);
+            $table->timestamps();
+            $table->string('created_by', 150);
+            $table->string('updated_by', 150);
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+        }
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('presensi');
+    }
+}
